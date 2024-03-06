ae1e0844bcc742a452044a1753762d6a3cc35ee2
René Mihula <mihula@provys.com>
2024-03-06 17:13:45 +0100

    empty project

---
+++ src/Program.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Diagnostics;
+using ClosedXML.Excel;
+
+namespace Provys.Standard.LibCx
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+        }
+    }
+}
